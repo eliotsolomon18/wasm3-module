9ed6ac4e5da15de4af7e4a033638293aac048cad
Eliot Solomon <ehsolomo@cs.cmu.edu>
2024-11-28 19:08:03 -0600

    Illustrate how to expose a function to the WASM program

--- wasm/two.c
+++ wasm/two.c
@@ -1,5 +1,13 @@
-int
+#include <stdint.h>
+
+int32_t print_int(int32_t i) __attribute__((
+    __import_module__("custom"),
+    __import_name__("print_int")
+));
+
+int32_t
 two()
 {
+    print_int(3);
     return (2);
 }
