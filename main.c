40a55fe3037de4eaf292890f282ce6fcc747e384
Eliot Solomon <ehsolomo@cs.cmu.edu>
2024-12-07 11:49:46 -0600

    Implement per-CPU runtimes

--- main.c
+++ main.c
@@ -23,26 +23,30 @@
 
 #define WASM_STACK_SIZE (64 * 1024) // 64 KB stack
 
+// A runtime and its associated metadata
+struct wasm_runtime {
+    spinlock_t lock; // The Wasm3 runtime lock
+    IM3Runtime runtime; // The Wasm3 runtime
+    IM3Module module; // The Wasm3 module
+    IM3Function alloc_func; // The allocation function within the Wasm3 module
+    IM3Function filter_func; // The filter function within the Wasm3 module
+    struct packet_header *header; // A pointer to the packer header on WASM runtime's heap
+};
+
 // The Wasm3 environment shared by all runtimes
 IM3Environment env = NULL;
 
-// The Wasm3 runtime lock
-DEFINE_SPINLOCK(lock);
+// The reconfiguration lock
+DEFINE_SPINLOCK(reconf_lock);
 
-// The Wasm3 runtime
-IM3Runtime runtime = NULL;
+// WASM code buffer
+char *wasm_code = NULL;
 
-// The Wasm3 module
-IM3Module module = NULL;
+// Size of WASM code buffer
+unsigned int wasm_size = 0;
 
-// The allocation function within the Wasm3 module
-IM3Function alloc_func = NULL;
-
-// The filter function within the Wasm3 module
-IM3Function filter_func = NULL;
-
-// Pointer to packer header on WASM runtime's heap
-struct packet_header *header;
+// A pointer to the array of Wasm3 runtimes
+struct wasm_runtime *runtimes;
 
 // Device number
 static dev_t dev_num;
@@ -64,12 +68,6 @@ static struct file_operations fops = {
     .owner = THIS_MODULE,
     .write = cdev_write,
 };
-
-// WASM code buffer
-char *wasm_code = NULL;
-
-// Size of WASM code buffer
-unsigned int wasm_size = 0;
 
 // Netfilter hook packet handler
 static unsigned int nf_filter(void *priv, struct sk_buff *skb, const struct nf_hook_state *state);
@@ -106,6 +104,17 @@ wasm_init(void)
     if ((env = m3_NewEnvironment()) == NULL) {
         pr_err("Failed to create Wasm3 environment.\n");
         return -1;
+    }
+
+    // Allocate the array of Wasm3 runtimes.
+    if ((runtimes = kcalloc(nr_cpu_ids, sizeof(struct wasm_runtime), GFP_KERNEL)) == NULL) {
+        pr_err("Failed to allocate runtime array.\n");
+        return -1;
+    }
+
+    // Initialize the lock for each runtime.
+    for (int cpu = 0; cpu < nr_cpu_ids; cpu++) {
+        spin_lock_init(&runtimes[cpu].lock);
     }
 
     // Register the device class.
@@ -161,146 +170,165 @@ wasm_init(void)
 }
 
 /*
+ * Clean up after a failed reconfiguration attempt.
+ */
+static void
+reconfigure_abort(int fail_cpu, char *new_wasm_code, struct wasm_runtime *new_runtimes)
+{
+    // Free every new runtime that has been created up to this point.
+    for (int cpu = 0; cpu <= fail_cpu; cpu++) {
+        if (new_runtimes[cpu].runtime != NULL) {
+            m3_FreeRuntime(new_runtimes[cpu].runtime);
+        }
+    }
+
+    // Free the temporary array of new runtimes.
+    kfree(new_runtimes);
+
+    // Free the new code buffer.
+    kfree(new_wasm_code);
+}
+
+/*
  * Handle a write to the character device.
  */
 static ssize_t
 cdev_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
 {
-    // Acquire the runtime lock.
-    unsigned long flags;
-    spin_lock_irqsave(&lock, flags);
-
-    // Free the current runtime if it exists.
-    if (runtime != NULL) {
-        alloc_func = NULL;
-        filter_func = NULL;
-        m3_FreeRuntime(runtime);
-        runtime = NULL;
-    }
-
-    // Free the current code buffer if it exists.
-    if (wasm_code != NULL) {
-        kfree(wasm_code);
-        wasm_code = NULL;
-        wasm_size = 0;
-    }
+    // Acquire the reconfiguration lock.
+    unsigned long reconf_flags;
+    spin_lock_irqsave(&reconf_lock, reconf_flags);
 
     // Allocate a new code buffer.
-    if ((wasm_code = kmalloc(len, GFP_KERNEL)) == NULL) {
-        pr_err("Failed to allocate buffer of length %zu bytes.\n", len);
-        spin_unlock_irqrestore(&lock, flags);
+    char *new_wasm_code;
+    if ((new_wasm_code = kmalloc(len, GFP_KERNEL)) == NULL) {
+        pr_err("Failed to allocate code buffer of length %zu bytes.\n", len);
+        spin_unlock_irqrestore(&reconf_lock, reconf_flags);
         return -ENOMEM;
     }
 
     // Copy the code from user space into the buffer.
-    if (copy_from_user(wasm_code, buf, len) != 0) {
-        pr_err("Failed to copy data from user space.\n");
-        kfree(wasm_code);
-        wasm_code = NULL;
-        spin_unlock_irqrestore(&lock, flags);
+    if (copy_from_user(new_wasm_code, buf, len) != 0) {
+        pr_err("Failed to copy code from user space.\n");
+        kfree(new_wasm_code);
+        spin_unlock_irqrestore(&reconf_lock, reconf_flags);
         return -EFAULT;
     }
+
+    // Allocate a temporary array to store the new runtimes.
+    struct wasm_runtime *new_runtimes;  
+    if ((new_runtimes = kcalloc(nr_cpu_ids, sizeof(struct wasm_runtime), GFP_KERNEL)) == NULL) {
+        pr_err("Failed to allocate new runtime array.\n");
+        kfree(new_wasm_code);
+        spin_unlock_irqrestore(&reconf_lock, reconf_flags);
+        return -ENOMEM;
+    }
+
+    // Set up all of the new runtimes.
+    for (int cpu = 0; cpu < nr_cpu_ids; cpu++) {
+        // Initialize the Wasm3 runtime.
+        if ((new_runtimes[cpu].runtime = m3_NewRuntime(env, WASM_STACK_SIZE, NULL)) == NULL) {
+            pr_err("Failed to create new runtime.\n");
+            reconfigure_abort(cpu, new_wasm_code, new_runtimes);
+            spin_unlock_irqrestore(&reconf_lock, reconf_flags);
+            return -ENOMEM;
+        }
+
+        // Parse the WASM module.
+        M3Result result;
+        if ((result = m3_ParseModule(env, &new_runtimes[cpu].module, new_wasm_code, len)) != NULL) {
+            pr_err("Failed to parse module: %s.\n", result);
+            reconfigure_abort(cpu, new_wasm_code, new_runtimes);
+            spin_unlock_irqrestore(&reconf_lock, reconf_flags);
+            return -EINVAL;
+        }
+
+        // Load the WASM module.
+        if ((result = m3_LoadModule(new_runtimes[cpu].runtime, new_runtimes[cpu].module)) != NULL) {
+            pr_err("Failed to load module: %s.\n", result);
+            reconfigure_abort(cpu, new_wasm_code, new_runtimes);
+            spin_unlock_irqrestore(&reconf_lock, reconf_flags);
+            return -ENOMEM;
+        }
+
+        // Link the print_int() function to the module.
+        if ((result = m3_LinkRawFunction(new_runtimes[cpu].module, "custom", "print_int", "i(i)", &print_int)) != NULL) {
+            pr_err("Failed to link print_int() to module: %s.\n", result);
+        }
+
+        // Find the alloc() WASM function in the module.`
+        if ((result = m3_FindFunction(&new_runtimes[cpu].alloc_func, new_runtimes[cpu].runtime, "alloc")) != NULL) {
+            pr_err("Error finding alloc() in module: %s.\n", result);
+            reconfigure_abort(cpu, new_wasm_code, new_runtimes);
+            spin_unlock_irqrestore(&reconf_lock, reconf_flags);
+            return -EINVAL;
+        }
+
+        // Find the filter() WASM function in the module.
+        if ((result = m3_FindFunction(&new_runtimes[cpu].filter_func, new_runtimes[cpu].runtime, "filter")) != NULL) {
+            pr_err("Error finding filter() in module: %s.\n", result);
+            reconfigure_abort(cpu, new_wasm_code, new_runtimes);
+            spin_unlock_irqrestore(&reconf_lock, reconf_flags);
+            return -EINVAL;
+        }
+
+        // Call the alloc() function.
+        if ((result = m3_CallV(new_runtimes[cpu].alloc_func, sizeof(struct packet_header))) != NULL) {
+            pr_err("Error calling alloc(): %s.\n", result);
+            reconfigure_abort(cpu, new_wasm_code, new_runtimes);
+            spin_unlock_irqrestore(&reconf_lock, reconf_flags);
+            return -EINVAL;
+        }
+
+        // Fetch the alloc() return value.
+        uint64_t alloc_val = 0;
+        if ((result = m3_GetResultsV(new_runtimes[cpu].alloc_func, &alloc_val)) != NULL) {
+            pr_err("Error getting results from alloc(): %s.\n", result);
+            reconfigure_abort(cpu, new_wasm_code, new_runtimes);
+            spin_unlock_irqrestore(&reconf_lock, reconf_flags);
+            return -EINVAL;
+        }
+
+        // Compute a pointer to the allocated region.
+        new_runtimes[cpu].header = (struct packet_header *)(m3_GetMemory(new_runtimes[cpu].runtime, NULL, 0) + alloc_val);
+    }
+
+    // Install all of the new runtimes.
+    for (int cpu = 0; cpu < nr_cpu_ids; cpu++) {
+        // Acquire the runtime lock.
+        unsigned long runtime_flags;
+        spin_lock_irqsave(&runtimes[cpu].lock, runtime_flags);
+
+        // Free the current runtime if it exists.
+        if (runtimes[cpu].runtime != NULL) {
+            m3_FreeRuntime(runtimes[cpu].runtime);
+        }
+
+        // Copy the new pointers into the actual runtime array.
+        runtimes[cpu].runtime = new_runtimes[cpu].runtime;
+        runtimes[cpu].module = new_runtimes[cpu].module;
+        runtimes[cpu].alloc_func = new_runtimes[cpu].alloc_func;
+        runtimes[cpu].filter_func = new_runtimes[cpu].filter_func;
+        runtimes[cpu].header = new_runtimes[cpu].header;
+
+        // Release the runtime lock.
+        spin_unlock_irqrestore(&runtimes[cpu].lock, runtime_flags);
+    }
+
+    // Free the temporary array of new runtimes.
+    kfree(new_runtimes);
+
+    // Free the old code buffer if it exists.
+    if (wasm_code != NULL) {
+        kfree(wasm_code);
+    }
+
+    // Save the new code buffer.
+    wasm_code = new_wasm_code;
     wasm_size = len;
 
-    // Initialize the Wasm3 runtime.
-    if ((runtime = m3_NewRuntime(env, WASM_STACK_SIZE, NULL)) == NULL) {
-        pr_err("Failed to create Wasm3 runtime.\n");
-        kfree(wasm_code);
-        wasm_code = NULL;
-        wasm_size = 0;
-        spin_unlock_irqrestore(&lock, flags);
-        return -ENOMEM;
-    }
-
-    // Parse the WASM module.
-    M3Result result;
-    if ((result = m3_ParseModule(env, &module, wasm_code, wasm_size)) != NULL) {
-        pr_err("Failed to parse module: %s.\n", result);
-        m3_FreeRuntime(runtime);
-        runtime = NULL;
-        kfree(wasm_code);
-        wasm_code = NULL;
-        wasm_size = 0;
-        spin_unlock_irqrestore(&lock, flags);
-        return -EINVAL;
-    }
-
-    // Load the WASM module.
-    if ((result = m3_LoadModule(runtime, module)) != NULL) {
-        pr_err("Failed to load module: %s.\n", result);
-        m3_FreeRuntime(runtime);
-        runtime = NULL;
-        kfree(wasm_code);
-        wasm_code = NULL;
-        wasm_size = 0;
-        spin_unlock_irqrestore(&lock, flags);
-        return -ENOMEM;
-    }
-
-    // Link the print_int() function to the module.
-    if ((result = m3_LinkRawFunction(module, "custom", "print_int", "i(i)", &print_int)) != NULL) {
-        pr_err("Failed to link print_int() to module: %s.\n", result);
-    }
-
-    // Find the alloc() WASM function in the module.`
-    if ((result = m3_FindFunction(&alloc_func, runtime, "alloc")) != NULL) {
-        pr_err("Error finding alloc() in module: %s.\n", result);
-        m3_FreeRuntime(runtime);
-        runtime = NULL;
-        kfree(wasm_code);
-        wasm_code = NULL;
-        wasm_size = 0;
-        spin_unlock_irqrestore(&lock, flags);
-        return -EINVAL;
-    }
-
-    // Find the filter() WASM function in the module.
-    if ((result = m3_FindFunction(&filter_func, runtime, "filter")) != NULL) {
-        pr_err("Error finding filter() in module: %s.\n", result);
-        alloc_func = NULL;
-        m3_FreeRuntime(runtime);
-        runtime = NULL;
-        kfree(wasm_code);
-        wasm_code = NULL;
-        wasm_size = 0;
-        spin_unlock_irqrestore(&lock, flags);
-        return -EINVAL;
-    }
-
-    // Call the alloc() function.
-    if ((result = m3_CallV(alloc_func, sizeof(struct packet_header))) != NULL) {
-        pr_err("Error calling alloc(): %s.\n", result);
-        alloc_func = NULL;
-        filter_func = NULL;
-        m3_FreeRuntime(runtime);
-        runtime = NULL;
-        kfree(wasm_code);
-        wasm_code = NULL;
-        wasm_size = 0;
-        spin_unlock_irqrestore(&lock, flags);
-        return -EINVAL;
-    }
-
-    // Fetch the alloc() return value.
-    uint64_t alloc_val = 0;
-    if ((result = m3_GetResultsV(alloc_func, &alloc_val)) != NULL) {
-        pr_err("Error getting results from alloc(): %s.\n", result);
-        alloc_func = NULL;
-        filter_func = NULL;
-        m3_FreeRuntime(runtime);
-        runtime = NULL;
-        kfree(wasm_code);
-        wasm_code = NULL;
-        wasm_size = 0;
-        spin_unlock_irqrestore(&lock, flags);
-        return -EINVAL;
-    }
-
-    // Compute a pointer to the allocated region.
-    header = (struct packet_header *)(m3_GetMemory(runtime, NULL, 0) + alloc_val);
-
-    // Release the runtime lock.
-    spin_unlock_irqrestore(&lock, flags);
+    // Release the reconfiguration lock.
+    spin_unlock_irqrestore(&reconf_lock, reconf_flags);
 
     pr_info("Successfully loaded %zu bytes of WASM code.\n", wasm_size);
 
@@ -313,58 +341,66 @@ cdev_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
 static unsigned int
 nf_filter(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
+    // Disable preemption and get the ID of the current CPU.
+    int cpu = get_cpu();
+
     // Do nothing if no filter function exists.
-    if (filter_func == NULL) {
+    if (runtimes[cpu].filter_func == NULL) {
+        put_cpu();
         return NF_ACCEPT;
     }
 
-    // TODO: use multiple runtimes to avoid serializing packet processing
-
     // Acquire the runtime lock.
     unsigned long flags;
-    spin_lock_irqsave(&lock, flags);
+    spin_lock_irqsave(&runtimes[cpu].lock, flags);
 
     // Copy a portion of the packet headers into the runtime's memory.
     struct iphdr *ip_h = (struct iphdr *)skb_network_header(skb);
-    header->src_ip = ntohl(ip_h->saddr);
-    header->dst_ip = ntohl(ip_h->daddr);
-    header->len = ntohs(ip_h->tot_len);
+    runtimes[cpu].header->src_ip = ntohl(ip_h->saddr);
+    runtimes[cpu].header->dst_ip = ntohl(ip_h->daddr);
+    runtimes[cpu].header->len = ntohs(ip_h->tot_len);
     switch (ip_h->protocol) {
         case IPPROTO_TCP:
-            header->prot = TCP;
+            runtimes[cpu].header->prot = TCP;
             struct tcphdr *tcp_h = tcp_hdr(skb);
-            header->src_pt = ntohs(tcp_h->source);
-            header->dst_pt = ntohs(tcp_h->dest);
+            runtimes[cpu].header->src_pt = ntohs(tcp_h->source);
+            runtimes[cpu].header->dst_pt = ntohs(tcp_h->dest);
             break;
         case IPPROTO_UDP:
-            header->prot = UDP;
+            runtimes[cpu].header->prot = UDP;
             struct udphdr *udp_h = udp_hdr(skb);
-            header->src_pt = ntohs(udp_h->source);
-            header->dst_pt = ntohs(udp_h->dest);
+            runtimes[cpu].header->src_pt = ntohs(udp_h->source);
+            runtimes[cpu].header->dst_pt = ntohs(udp_h->dest);
             break;
         default:
-            spin_unlock_irqrestore(&lock, flags);
+            spin_unlock_irqrestore(&runtimes[cpu].lock, flags);
+            put_cpu();
             return NF_ACCEPT;
     }
 
     // Call the filter() function.
     M3Result result;
-    if ((result = m3_CallV(filter_func)) != NULL) {
+    if ((result = m3_CallV(runtimes[cpu].filter_func)) != NULL) {
         pr_err("Error calling filter(): %s.\n", result);
-        spin_unlock_irqrestore(&lock, flags);
+        spin_unlock_irqrestore(&runtimes[cpu].lock, flags);
+        put_cpu();
         return NF_ACCEPT;
     }
 
     // Fetch the filter() return value.
     uint32_t filter_val = 0;
-    if ((result = m3_GetResultsV(filter_func, &filter_val)) != NULL) {
+    if ((result = m3_GetResultsV(runtimes[cpu].filter_func, &filter_val)) != NULL) {
         pr_err("Error getting results from filter(): %s.\n", result);
-        spin_unlock_irqrestore(&lock, flags);
+        spin_unlock_irqrestore(&runtimes[cpu].lock, flags);
+        put_cpu();
         return NF_ACCEPT;
     }
 
     // Release the runtime lock.
-    spin_unlock_irqrestore(&lock, flags);
+    spin_unlock_irqrestore(&runtimes[cpu].lock, flags);
+
+    // Enable preemption.
+    put_cpu();
 
     // The return value specified what should be done with the packet.
     return filter_val;
@@ -376,24 +412,39 @@ nf_filter(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 static void __exit
 wasm_cleanup(void)
 {
-    // Unregister the netfilter hook.
-    nf_unregister_net_hook(&init_net, &nfho);
-
-    // Free the Wasm3 runtime if it exists.
-    if (runtime != NULL) {
-        m3_FreeRuntime(runtime);
-    }
-
-    // Free the code buffer if it exists.
-    if (wasm_code != NULL) {
-        kfree(wasm_code);
-    }
-
     // Free the character device.
     device_destroy(dev_class, dev_num);
     cdev_del(&cdev);
     unregister_chrdev_region(dev_num, 1);
     class_destroy(dev_class);
+
+    // Unregister the netfilter hook.
+    nf_unregister_net_hook(&init_net, &nfho);
+
+    // Free all of the Wasm3 runtimes.
+    for (int cpu = 0; cpu < nr_cpu_ids; cpu++) {
+        // Acquire the runtime lock.
+        unsigned long flags;
+        spin_lock_irqsave(&runtimes[cpu].lock, flags);
+
+        // Free the runtime if it exists.
+        if (runtimes[cpu].runtime != NULL) {
+            m3_FreeRuntime(runtimes[cpu].runtime);
+            runtimes[cpu].module = NULL;
+            runtimes[cpu].alloc_func = NULL;
+            runtimes[cpu].filter_func = NULL;
+            runtimes[cpu].header = NULL;
+        }
+        
+        // Release the runtime lock.
+        spin_unlock_irqrestore(&runtimes[cpu].lock, flags);
+    }
+
+    // Free the code buffer if it exists.
+    if (wasm_code != NULL) {
+        kfree(wasm_code);
+        wasm_size = 0;
+    }
 
     // Free the Wasm3 environment.
     m3_FreeEnvironment(env);
